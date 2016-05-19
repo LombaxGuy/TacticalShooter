6c8fc69d87501a4de4285010fc9221a838c9c327
Mikkel Sommer Kristensen <mikkelsk64@hotmail.com>
2016-05-19 14:28:22 +0200

    Revert "Updated the camera to make it stay inside the map. Also added the UI to the game level."
    This reverts commit cba8afdaf3e4c4e90ae0a81de418429a42e6306e.

--- Source/TacticalShooter/TopDownCameraPawn.cpp
+++ Source/TacticalShooter/TopDownCameraPawn.cpp
@@ -118,32 +118,20 @@ void ATopDownCameraPawn::Tick(float deltaSeconds)
 
 		if (mousePosition.X < cameraScrollBoundary)
 		{
-			if (GetActorLocation().Y < 4500)
-			{
-				MoveCameraRight(-1.0f);
-			}
+			MoveCameraRight(-1.0f);
 		}
 		else if (mousePosition.X > viewportSize.X - cameraScrollBoundary)
 		{
-			if (GetActorLocation().Y > -4500)
-			{
-				MoveCameraRight(1.0f);
-			}
+			MoveCameraRight(1.0f);
 		}
 
 		if (mousePosition.Y < cameraScrollBoundary)
 		{
-			if (GetActorLocation().X > -4800)
-			{
-				MoveCameraForward(1.0f);
-			}
+			MoveCameraForward(1.0f);
 		}
 		else if (mousePosition.Y > viewportSize.Y - cameraScrollBoundary)
 		{
-			if (GetActorLocation().X < 4700)
-			{
-				MoveCameraForward(-1.0f);
-			}
+			MoveCameraForward(-1.0f);
 		}
 		
 	}
